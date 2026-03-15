ba53cde849537c4101287a9bb1c1cbe37d2561b6
swaroopa777 <swaroopaet@gmail.com>
2026-03-15 23:40:19 +0530

    Final Commit

--- part2/countries/src/services/weather.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const apiKey = import.meta.env.VITE_SOME_KEY
-
-const getWeather = capital =>
-  axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`
-  ).then(res => res.data)
-
-export default { getWeather }
